9776f37e30207d126d2fd59104b38bf9af2c21f5
Manish Budhiraja <22340444+imbudhiraja@users.noreply.github.com>
2020-09-03 10:57:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
     node: true
   },
-  parser: "babel-eslint",
+  parser: "@babel/eslint-parser",
   plugins: ["filenames", "immutable"],
   rules: {
     "array-bracket-spacing": "error",
